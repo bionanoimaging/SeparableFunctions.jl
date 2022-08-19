bcde4ab4958351d730416704376618320f0a54ae
RainerHeintzmann <heintzmann@gmail.com>
2022-08-19 13:11:58 +0200

    implemented the generic mechanism to compile lots of functions

--- performance_tests/test_performance.jl
+++ performance_tests/test_performance.jl
@@ -2,18 +2,33 @@ using SeparableFunctions
 
 sz = (2000,1900)
 sigma = (150.4,220.3)
+# Create some functions by hand
 fct = (r, pos, sigma)-> exp(-(r-pos)^2/(2*sigma^2))
 @time my_gaussian = separable_view(fct, sz, (0.1,0.2), sigma);
 @time q = collect(my_gaussian); # 0.004
 
 @time w = create_separable(fct, sz, (0.1,0.2),sigma); # 0.004
+
+# Test some predefined functions
+@time my_gaussian = gaussian_sep_lz(Array{Float64}, sz, sigma);
+@time my_gaussian = gaussian_sep_lz(sz, sigma);
+@time q = collect(my_gaussian); # 0.002 sec
+
+fct = (r, pos, sigma)-> exp(-(r-pos)^2/(2*sigma^2))
+@time q = collect((prod(fct.(Tuple(c), (0.0,0.0), sigma)) for c in CartesianIndices(sz)));
+
+using IndexFunArrays
+@time q = collect(gaussian(sz, sigma=sigma));
+@time e = collect(exp_ikx(sz, scale=(1.1,2.2))); # 0.19
+@time e = exp_ikx_sep(sz, (1.1,2.2)); # 0.006
+
 # @vt q w
 
-using CUDA
-CUDA.@time c_my_gaussian = separable_view(CuArray{Float32}, fct, sz, (0.1,0.2), sigma);
-CUDA.@time cq = collect(c_my_gaussian); # 0.004
+# using CUDA
+# CUDA.@time c_my_gaussian = separable_view(CuArray{Float32}, fct, sz, (0.1,0.2), sigma);
+# CUDA.@time cq = collect(c_my_gaussian); # 0.004
 
-@time w = create_separable(CuArray{Float32}, fct, sz, (0.1,0.2), sigma); # 0.004
+# @time w = create_separable(CuArray{Float32}, fct, sz, (0.1,0.2), sigma); # 0.004
 
 # ca = CuArray([1 2 3 4 5]);
 # cb = CuArray([6 7 8 9 10]);
--- src/specific.jl
+++ src/specific.jl
@@ -20,22 +20,63 @@ julia> my_gaussian = gaussian_sep((6,5), sigma, pos)
  0.0871608    0.477114     0.960789     0.71177      0.19398
  0.0175975    0.0963276    0.19398      0.143704     0.0391639```
 """
-function gaussian_sep(::Type{TA}, sz::NTuple{N, Int}, sigma, pos=zeros(Float32,N); center=sz.÷2 .+1) where {TA, N}
-    invsigma22 = 1 ./(2 .*sigma.^2)
-    fct = (r, invsigma22, pos) -> exp(-(r-pos)^2*invsigma22)
-    separable_create(TA, fct, sz, invsigma22, pos; center=center)
+gaussian_sep
+# function gaussian_sep(::Type{TA}, sz::NTuple{N, Int}, sigma, pos=zeros(Float32,N); center=sz.÷2 .+1) where {TA, N}
+#     invsigma22 = 1 ./(2 .*sigma.^2)
+#     fct = (r, invsigma22, pos) -> exp(-(r-pos)^2*invsigma22)
+#     separable_create(TA, fct, sz, invsigma22, pos; center=center)
+# end
+
+# function gaussian_sep(sz::NTuple{N, Int}, sigma, pos=zeros(Float32,N); center=sz.÷2 .+1) where {N}
+#     gaussian_sep(DefaultArrType, sz, sigma, pos; center=center)
+# end
+
+# function gaussian_sep_lz(::Type{TA}, sz::NTuple{N, Int}, sigma, pos=zeros(Float32,N); center=sz.÷2 .+1) where {TA, N}
+#     invsigma22 = 1 ./(2 .*sigma.^2)
+#     fct = (r, invsigma22, pos) -> exp(-(r-pos)^2*invsigma22)
+#     separable_view(TA, fct, sz, invsigma22, pos; center=center)
+# end
+
+# function gaussian_sep_lz(sz::NTuple{N, Int}, sigma, pos=zeros(Float32,N); center=sz.÷2 .+1) where {N}
+#     gaussian_sep_lz(DefaultArrType, sz, sigma, pos; center=center)
+# end
+
+function generate_functions_expr()
+    # offset and scale is already wrapped in the generator function
+    # x_expr = :(scale .* (x .- offset))
+
+    functions = [
+        # function_name, extra_vars,  function_expression:
+        (:(gaussian), (:sigma,), :((x,pos,sigma) -> exp(-(x-pos)^2/(2 .* sigma^2))), Float32),
+        (:(sinc), (:scale,), :((x,pos,scale) -> sinc((x-pos)/scale)), Float32),
+        (:(exp_ikx), (:k,), :((x,pos,k) -> cis((x-pos)*k)), ComplexF32),
+    ]
+    return functions
 end
 
-function gaussian_sep(sz::NTuple{N, Int}, sigma, pos=zeros(Float32,N); center=sz.÷2 .+1) where {N}
-    gaussian_sep(DefaultArrType, sz, sigma, pos; center=center)
-end
+for F in generate_functions_expr() 
+    # default functions with offset and scaling behavior
 
-function gaussian_sep_lz(::Type{TA}, sz::NTuple{N, Int}, sigma, pos=zeros(Float32,N); center=sz.÷2 .+1) where {TA, N}
-    invsigma22 = 1 ./(2 .*sigma.^2)
-    fct = (r, invsigma22, pos) -> exp(-(r-pos)^2*invsigma22)
-    separable_view(TA, fct, sz, invsigma22, pos; center=center)
-end
+    @eval function $(Symbol(F[1], :_sep))(::Type{TA}, sz::NTuple{N, Int}, $(F[2]...), pos=zeros(Float32, N); center=sz.÷2 .+1) where {TA, N}
+        fct2 = $(F[3]) # to assign the function to a symbol
+        separable_create(TA, fct2, sz, pos, $(F[2]...); center=center)
+    end
 
-function gaussian_sep_lz(sz::NTuple{N, Int}, sigma, pos=zeros(Float32,N); center=sz.÷2 .+1) where {N}
-    gaussian_sep_lz(DefaultArrType, sz, sigma, pos; center=center)
-end
+    @eval function $(Symbol(F[1], :_sep))(sz::NTuple{N, Int}, $(F[2]...), pos=zeros(Float32, N); center=sz.÷2 .+1) where {N}
+        fct1 = $(F[3]) # to assign the function to a symbol
+        separable_create(Array{$(F[4])}, fct1, sz, pos, $(F[2]...); center=center)
+    end
+
+    @eval function $(Symbol(F[1], :_sep_lz))(::Type{TA}, sz::NTuple{N, Int}, $(F[2]...), pos=zeros(Float32, N); center=sz.÷2 .+1) where {TA, N}
+        fct4 = $(F[3]) # to assign the function to a symbol
+        separable_view(TA, fct4, sz, pos, $(F[2]...); center=center)
+    end
+    
+    @eval function $(Symbol(F[1], :_sep_lz))(sz::NTuple{N, Int}, $(F[2]...), pos=zeros(Float32, N); center=sz.÷2 .+1) where {N}
+        fct3 = $(F[3]) # to assign the function to a symbol
+        separable_view(Array{$(F[4])}, fct3, sz, pos, $(F[2]...); center=center)
+    end
+
+    @eval export $(Symbol(F[1], :_sep))
+    @eval export $(Symbol(F[1], :_sep_lz))
+end 
